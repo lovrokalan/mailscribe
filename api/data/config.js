cc553ed4edcb3d4146f78a4b827e3395ca56364c
Lovro Kalan <lovro.kalan1@gmail.com>
2020-04-21 16:06:13 +0200

    Basic CRUD api

---
+++ api/data/config.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+// Set database connection credentials
+const config = {
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'first-api',
+  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+};
+
+// Create a MySQL pool
+const pool = mysql.createPool(config);
+
+// Export the pool
+module.exports = pool;
